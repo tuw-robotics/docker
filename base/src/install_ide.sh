2e5604c2c799f157570591e7cc09c037bf147fcd
Eugen <eugen.kaltenegger@gmail.com>
2021-12-23 08:52:40 +0100

    apt  -> apt-get

--- base/src/install_ide.sh
+++ base/src/install_ide.sh
@@ -5,8 +5,8 @@ set -e
 echo "Install Kdevelop and VSCode"
 
 # VS Code 
-apt install -y apt-transport-https software-properties-common wget kdevelop geany 
+apt-get install -y apt-transport-https software-properties-common wget kdevelop geany 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt-get update
-apt install -y code
+apt-get install -y code
