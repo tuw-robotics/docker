3b78a6abedaf6cbe8caa6ff12069859a6932aaa6
Markus Bader <markus.bader@tuwien.ac.at>
2022-08-24 13:26:57 +0200

    in progress mpn

--- mpn/melodic/mpn/src/setup_ws.sh
+++ mpn/melodic/mpn/src/setup_ws.sh
@@ -9,19 +9,44 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 # create and build workspace for general packages
 mkdir -p ${PROJECT_ROOT}/ws00/src
 cd ${PROJECT_ROOT}/ws00/src  && \
-	git clone https://github.com/tuw-robotics/tuw_geometry.git  && \
-	git clone https://github.com/tuw-robotics/tuw_msgs.git 
+	git clone git@github.com:tuw-robotics/grid_map.git  && \
+	git clone git@github.com:tuw-robotics/tuw_msgs.git
 
 cd ${PROJECT_ROOT}/ws00 && catkin_make
 source ${PROJECT_ROOT}/ws00/devel/setup.bash
 echo 'source ${PROJECT_ROOT}/ws00/devel/setup.bash' >> ~/.bashrc
 
-# create and build worksprace for IWOS specific packages
+# create and build worksprace 
 mkdir -p ${PROJECT_ROOT}/ws01/src
 cd ${PROJECT_ROOT}/ws01/src && \
-	git clone https://github.com/tuw-robotics/tuw_multi_robot.git
+	git git@github.com:tuw-robotics/tuw_rviz.git
+	git git@github.com:tuw-robotics/tuw_agv.git
+	git git@github.com:tuw-robotics/tuw_control.git
+	git git@github.com:tuw-robotics/tuw_geometry.git
+	git git@github.com:tuw-robotics/tuw_teleop.git
+	git git@github.com:tuw-robotics/tuw_collision_alarm.git
+	git git@github.com:tuw-robotics/tuw_global_planner.git
 
 cd ${PROJECT_ROOT}/ws01 && catkin_make
 source ${PROJECT_ROOT}/ws01/devel/setup.bash
 echo 'source ${PROJECT_ROOT}/ws01/devel/setup.bash' >> ~/.bashrc
 
+
+# create and build worksprace 
+mkdir -p ${PROJECT_ROOT}/ws02/src
+cd ${PROJECT_ROOT}/ws02/src && \
+	git git@github.com:tuw-robotics/tuw_multi_robot.git
+
+cd ${PROJECT_ROOT}/ws02 && catkin_make
+source ${PROJECT_ROOT}/ws02/devel/setup.bash
+echo 'source ${PROJECT_ROOT}/ws02/devel/setup.bash' >> ~/.bashrc
+
+
+# create and build worksprace 
+mkdir -p ${PROJECT_ROOT}/ws02/src
+cd ${PROJECT_ROOT}/ws02/src && \
+	git git@github.com:tuw-robotics/tuw_multi_robot.git
+
+cd ${PROJECT_ROOT}/ws02 && catkin_make
+source ${PROJECT_ROOT}/ws02/devel/setup.bash
+echo 'source ${PROJECT_ROOT}/ws02/devel/setup.bash' >> ~/.bashrc
