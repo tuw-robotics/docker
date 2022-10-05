9b77d40458eb9e16bc77ae094bcac2b65febe0e1
Markus Bader <markus.bader@tuwien.ac.at>
2022-10-05 13:52:41 +0200

    nav2 added

--- enviroment/humble/core/config/bash/env.sh
+++ enviroment/humble/core/config/bash/env.sh
@@ -1,10 +1,6 @@
 #export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 #export RMW_IMPLEMENTATION=rmw_gurumdds_cpp
-#export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-
-#source /usr/share/colcon_cd/function/colcon_cd.sh
-#export _colcon_cd_root=/opt/ros/$ROS_DISTRO/
-#source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 source /opt/ros/$ROS_DISTRO/setup.bash
 echo "** ROS2 $ROS_DISTRO initialized with $RMW_IMPLEMENTATION**"
