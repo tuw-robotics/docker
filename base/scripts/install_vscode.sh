8f40fcc9f6bc6d4ac6fb31f1e865a14565374bfc
Markus Bader <markus.bader@tuwien.ac.at>
2023-02-21 12:56:32 +0100

    base working for humble and noetic

--- base/scripts/install_vscode.sh
+++ base/scripts/install_vscode.sh
@@ -4,12 +4,15 @@ set -e
 
 echo "Install VSCode"
 
+apt-get update
 apt-get install -y  wget apt-transport-https software-properties-common
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt-get update
+echo "apt-get install -y code"
 apt-get install -y code
 
+echo "install-extension ms-vscode"
 sudo -u ${MY_USER} code --install-extension ms-vscode.cpptools
 sudo -u ${MY_USER} code --install-extension ms-vscode.cmake-tools
 sudo -u ${MY_USER} code --install-extension ms-python.python
