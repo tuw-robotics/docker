e8e256399a57e612d9bbe026a9fde7fba6bd7dcf
Markus Bader <markus.bader@tuwien.ac.at>
2023-03-01 08:37:49 +0100

    run-script added

--- pathfollowing/noetic/run-image-on-r0.sh
+++ pathfollowing/noetic/run-image-on-r0.sh
@@ -3,7 +3,7 @@
 ROS_DISTRO=noetic
 OWNER=tuwrobotics
 PREFIX=tuw
-ROOT_CONTAINER=${OWNER}/${PREFIX}-${ROS_DISTRO}-ide
+PROJECT=pathfollowing
 HOSTNAME=$(hostname)
 IMAGE_NAME=${PREFIX}-${ROS_DISTRO}-${PROJECT}
 CONTAINER_NAME=${ROS_DISTRO}-${PROJECT}
@@ -15,5 +15,7 @@ docker run --entrypoint /bin/bash -ti --rm --privileged \
 	-v ./tmp/ws01/src:/home/robot/projects/ros/pathfollowing/ws01/src \
 	-v ./tmp/tuw/src:/home/robot/projects/ros/pathfollowing/tuw/src \
 	--network="host" --env="DISPLAY" \
-	--add-host "${CONTAINER_HOSTNAME}:127.0.0.1" --add-host "r0:10.76.11.50" --hostname ${CONTAINER_HOSTNAME} \
-	--device=/dev/ttyS0 --name ${CONTAINER_HOSTNAME} ${OWNER}/${IMAGE_NAME}
+	--add-host "${CONTAINER_HOSTNAME}:127.0.0.1" --add-host "r0:10.76.11.50" \
+	--hostname ${CONTAINER_HOSTNAME} \
+	--device=/dev/ttyS0 \
+	--name ${CONTAINER_HOSTNAME} ${OWNER}/${IMAGE_NAME}
